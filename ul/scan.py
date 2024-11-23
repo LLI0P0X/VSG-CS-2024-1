f2336ea2eeb20f2b754620c93d64a599cc6e1f6b
Ulyana0 <97434790+Ulyana0@users.noreply.github.com>
2024-11-23 19:07:47 +0300

    json

--- ul/scan.py
+++ ul/scan.py
@@ -1,37 +1,50 @@
 import nmap
-
+import json
 
 def scan_host(target):
     nm = nmap.PortScanner()
     nm.scan(target, arguments='-sV -O -sC -A -T4 -p-')
 
+    results = []
+
     for host in nm.all_hosts():
-        print(f"Host: {host}")
-        print(f"State: {nm[host].state()}")
+        host_info = {
+            "host": host,
+            "state": nm[host].state(),
+            "protocol": "tcp"
+        }
+        results.append(host_info)
 
         for proto in nm[host].all_protocols():
-            print(f"Protocol: {proto}")
             lport = nm[host][proto].keys()
             for port in sorted(lport):
-                print(f"Port: {port}")
-                print(f"State: {nm[host][proto][port]['state']}")
-                print(f"Service: {nm[host][proto][port]['name']}")
-                print(f"Product: {nm[host][proto][port]['product']}")
-                print(f"Version: {nm[host][proto][port]['version']}")
-
-                # Проверка на наличие ключа 'script'
-                if 'script' in nm[host][proto][port]:
-                    print(f"Script: {nm[host][proto][port]['script']}")
-                else:
-                    print("Script: No script information available")
+                port_info = {
+                    "port": port,
+                    "state": nm[host][proto][port]['state'],
+                    "service": nm[host][proto][port]['name'],
+                    "product": nm[host][proto][port]['product'],
+                    "version": nm[host][proto][port]['version'],
+                    "script": nm[host][proto][port]['script'] if 'script' in nm[host][proto][port] else "No script information available"
+                }
+                results.append(port_info)
 
         if 'osmatch' in nm[host]:
             for osmatch in nm[host]['osmatch']:
-                print(f"OS Match: {osmatch['name']}")
-                print(f"Accuracy: {osmatch['accuracy']}")
+                os_info = {
+                    "type": "OS Match",
+                    "name": osmatch['name'],
+                    "accuracy": osmatch['accuracy']
+                }
+                results.append(os_info)
 
-        print("-" * 60)
-
+    return results
 
 target = '127.0.0.1'
-scan_host(target)
+scan_results = scan_host(target)
+
+# Записываем результаты в файл в формате JSON
+with open('scan_results.json', 'w') as json_file:
+    json.dump(scan_results, json_file, indent=4)
+
+# Выводим результаты на экран
+print(json.dumps(scan_results, indent=4))
