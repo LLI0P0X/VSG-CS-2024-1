a0e3d46592512387f96c49e8a39c1befbe8e202f
LLI0P0X <laketc@list.ru>
2024-11-24 13:25:24 +0300

    Update PDF.py

--- PDF.py
+++ PDF.py
@@ -18,6 +18,7 @@ from myLogger import logger
 
 pdfmetrics.registerFont(TTFont('DejaVuSans', 'DejaVuSans.ttf'))
 
+
 def create_pdf(filename, text):
     # Регистрируем шрифт с поддержкой кириллицы
     pdfmetrics.registerFont(TTFont('DejaVuSans', 'DejaVuSans.ttf'))
@@ -51,37 +52,44 @@ async def cycle():
             header_style = styles['Heading1']
             header_style.alignment = 1
             header_style.fontName = 'DejaVuSans'  # Используем шрифт с поддержкой кириллицы
-            header = Paragraph(f"Отчет о безопасности по адресам {task[1]}-{task[2]}", header_style)
+            header = Paragraph(f"Отчет о безопасности по адресам {task[1]}-{task[2]}:", header_style)
             elements.append(header)
-            for report in await orm.get_reports_by_tid(task[0]):
-                logger.debug(report)
 
-                tid = report[1]
-                data.append([])
-                for item in report[2:]:
-                    data[len(data) - 1].append(item)
+            reports = await orm.get_reports_by_tid(task[0])
+            if len(reports):
+                for report in await orm.get_reports_by_tid(task[0]):
+                    logger.debug(report)
+                    tid = report[1]
+                    data.append([])
+                    for item in report[2:]:
+                        data[len(data) - 1].append(item)
 
-            # Создаем таблицу
-            table = Table(data)
-            style = TableStyle([
-                ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-                ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                ('FONTNAME', (0, 0), (-1, -1), 'DejaVuSans'),  # Используем шрифт с поддержкой кириллицы
-                ('BOTTOMPADDING', (0, 0), (-1, 0), 6),
-                ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-                ('GRID', (0, 0), (-1, -1), 1, colors.black)
-            ])
-            table.setStyle(style)
+                # Создаем таблицу
+                table = Table(data)
+                style = TableStyle([
+                    ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+                    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+                    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                    ('FONTNAME', (0, 0), (-1, -1), 'DejaVuSans'),  # Используем шрифт с поддержкой кириллицы
+                    ('BOTTOMPADDING', (0, 0), (-1, 0), 6),
+                    ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+                    ('GRID', (0, 0), (-1, -1), 1, colors.black)
+                ])
+                table.setStyle(style)
 
-            table._argW[0] = 1.3 * inch  # Ширина первого столбца
-            table._argW[1] = 0.8 * inch  # Ширина второго столбца
-            table._argW[2] = 0.8 * inch  # Ширина третьего столбца
-            table._argW[3] = 1.7 * inch  # Ширина четвертого столбца
-            table._argW[4] = 0.8 * inch  # Ширина пятого столбца
-            table._argW[5] = 2.9 * inch  # Ширина шестого столбца
+                table._argW[0] = 1.3 * inch  # Ширина первого столбца
+                table._argW[1] = 0.8 * inch  # Ширина второго столбца
+                table._argW[2] = 0.8 * inch  # Ширина третьего столбца
+                table._argW[3] = 1.7 * inch  # Ширина четвертого столбца
+                table._argW[4] = 0.8 * inch  # Ширина пятого столбца
+                table._argW[5] = 2.9 * inch  # Ширина шестого столбца
 
-            elements.append(table)
+                elements.append(table)
+            else:
+                logger.info("Нет отчетов для PDF")
+                header = Paragraph(f"Уязвимостей не обнаружено", header_style)
+                elements.append(header)
+
             # Создаем PDF-файл
             doc.build(elements)
 
